55e00bd2b2489f2a449f2de1fe99d0ad9fd789c3
user11 <user11@psdc-eda03.psdc.org.my>
2025-08-13 12:26:59 +0800

    coverage

--- UVM/design/tb/spi_cov.sv
+++ UVM/design/tb/spi_cov.sv
@@ -2,13 +2,13 @@ class spi_cov extends uvm_component;
   `uvm_component_utils(spi_cov)
     
   // Use implementation port to receive transactions
-	uvm_analysis_imp #(spi_tran, spi_cov) cov_imp;
+  uvm_analysis_imp #(spi_tran, spi_cov) cov_imp;
 
   function new(string name, uvm_component parent);
     super.new(name, parent);
     cov_imp = new("cov_imp", this);
     spi_cg = new();
-    spi_cg.set_inst_name($sformatf("%s\ (spi_cg\)", get_full_name()));
+    spi_cg.set_inst_name($sformatf("%s (spi_cg)", get_full_name()));
   endfunction
 
   // This will be called when transactions arrive
@@ -17,12 +17,37 @@ class spi_cov extends uvm_component;
   endfunction
 
   covergroup spi_cg with function sample(spi_tran tr);
-	// TODO: Fill in the necessary coverpoint
+  option.per_instance = 1;
+  option.comment = "THIS IS MY SPI_CG COVERAGE";
+
+  rst_n_cp:    coverpoint tr.rst_n;
+  start_cp:    coverpoint tr.start;
+  busy_cp:     coverpoint tr.busy;
+  done_cp:     coverpoint tr.done;
+  cs_n_cp:     coverpoint tr.cs_n;
+  tx_data_cp:  coverpoint tr.tx_data;
+  rx_data_cp:  coverpoint tr.rx_data;
+  miso_cp:     coverpoint tr.miso;
+  mosi_cp:     coverpoint tr.mosi;
+  init_cp: cross rst_n_cp,start_cp,busy_cp,done_cp,cs_n_cp;
+  data_cp: cross tx_data_cp, rx_data_cp;
+  ms_cp:   cross miso_cp, mosi_cp;
+
   endgroup
 
   function void report_phase(uvm_phase phase);
-    `uvm_info("COVERAGE spi_cg", $sformatf("Coverage spi_cg      : %.2f%%", spi_cg_inst.get_coverage()), UVM_NONE)
-	// TODO: Complete the message display info
-    //`uvm_info("COVERAGE spi_cg", $sformatf("Coverage addr_cp     : %.2f%%", spi_cg_inst.addr_cp.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage spi_cg       : %.2f%%", spi_cg.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage rst_n_cp       : %.2f%%", spi_cg.rst_n_cp.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage start_cp       : %.2f%%", spi_cg.start_cp.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage busy_cp        : %.2f%%", spi_cg.busy_cp.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage done_cp        : %.2f%%", spi_cg.done_cp.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage cs_n_cp        : %.2f%%", spi_cg.cs_n_cp.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage tx_data_cp      : %.2f%%", spi_cg.tx_data_cp.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage rx_data_cp      : %.2f%%", spi_cg.rx_data_cp.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage miso_cp        : %.2f%%", spi_cg.miso_cp.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage mosi_cp        : %.2f%%", spi_cg.mosi_cp.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage init_cp      : %.2f%%", spi_cg.init_cp.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage data_cp      : %.2f%%", spi_cg.data_cp.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage ms_cp        : %.2f%%", spi_cg.ms_cp.get_coverage()), UVM_NONE)
   endfunction
 endclass
--- UVM/design/tb/spi_tb.sv
+++ UVM/design/tb/spi_tb.sv
@@ -20,7 +20,7 @@ module spi_tb;
     `include "spi_agt0.sv"       // Agent0 class
     `include "spi_agt1.sv"       // Agent1 class
     `include "spi_scb.sv"       // Scoreboard class
-    //`include "spi_cov.sv"		// Coverage class
+    `include "spi_cov.sv"		// Coverage class
     `include "spi_env.sv"       // Env class
     `include "spi_test.sv"      // Test class
     `include "spi_test_sanity.sv"      // Test class
